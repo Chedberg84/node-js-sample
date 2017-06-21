d61d2d9bffa26cff0d2dbc5846dbaa37bdce6e6c
Chris Hedberg <christophhedberg@gmail.com>
2017-06-20 22:16:09 -0500

    Added a test for mongodb integration.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
+
+app.get('/test', function(request, response) {
+  response.send('This is a new route test');
+});
+
+app.post('/test', function(request, response) {
+  response.send('We received a test post');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+//apparently this is how you mongo
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/testing";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  db.collection("testing").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    db.close();
+  });
+});
